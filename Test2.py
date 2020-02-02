abacd8110a76d5e7830eb872f52e87b646aaf4eb
Victoria Li <victoria.li@berkeley.edu>
2020-02-01 21:40:01 -0800

    Fifth Commit

---
+++ Test2.py
@@ -0,0 +1,59 @@
+import requests, json
+
+apikey = "?apiKey=71dd2a0526f842549d117f89c0952f9a"
+urls = {'recipe':"https://api.spoonacular.com/recipes/findByIngredients"+apikey, 'substitutes' : "https://api.spoonacular.com/food/ingredients/substitutes"+apikey}
+
+headers = {'Content-Type': "application/json"}
+
+def get_valid_recipes(ingredients, amount=5):
+    #GIVEN a comma separated string of ingredient names
+    #RETURN a JSON list of AMOUNT number of recipes that can be made (represented as json dictionaries)
+    querystring = {"ingredients":ingredients, "number":amount}
+    response = requests.request("GET", urls['recipe'], headers=headers, params=querystring)
+    return response.json()
+
+def filter_incomplete_recipes(list):
+    #GIVEN a JSON list of recipes, RETURN a list of recipes (json dictionaries) that are complete
+    completerecipes = []
+    for recipe in list:
+        if recipe['missedIngredientCount'] == 0:
+            completerecipes.append(recipe)
+    return completerecipes
+
+def get_substitutes(ingredient):
+    #GIVEN a string name of an ingredient
+    #RETURN a list of subsitutes in the form of strings
+    querystring = {"ingredientName":ingredient}
+    response = requests.request("GET", urls['substitutes'], headers=headers, params=querystring)
+    return response.json()['substitutes']
+
+    
+
+
+#######################
+#PRINT FUNCTIONS: ONLY FOR TERMINAL TESTING PURPOSES
+
+def jprint(obj):
+    #PRINT a formatted string of a GIVEN Python JSON object
+    #I.E. : jprint(response.json())
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+def print_recipe_names(recipes):
+    #PRINT the names of recipes, GIVEN a list of recipe dictionaries
+    for recipe in recipes:
+        if recipe['missedIngredientCount'] == 0:
+            print('You have all the ingredients for ' + recipe['title'])
+        else:
+            missedIngredients = recipe['missedIngredients']
+            stringList = ''
+            for ingredient in missedIngredients:
+                stringList += ingredient['name'] + ', '
+            print('You need ' + stringList + 'to make ' + recipe['title'])
+
+def print_substitutes(ingredient):
+    #GIVEN a string name of an ingredient, PRINT out subsitutes
+    print('Possible substitutes for ' + ingredient + ':')
+    subs = get_substitutes(ingredient)
+    for substitute in subs:
+        print(substitute)
--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import render_template
 from flask import url_for
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
-from Test import *
+from Test2 import *
 
+# globally accessible list of ingredients
 global inputIngredientList
 inputIngredientList = []
 
@@ -15,42 +16,73 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/vickyli/frio/ingredien
 
 db = SQLAlchemy(app)
 
+# ingredients class with attributes id, text, and existance
 class Ingredients(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	text = db.Column(db.String(200))
 	gone = db.Column(db.Boolean)
 
+#home and intro page
 @app.route('/')
 def welcome():
 	return render_template('homepage.html')
 
+#mainpage route
 @app.route('/frio')
 def frio():
+	# ingredients is all ingredients in database
 	ingredients = Ingredients.query.all()
-	return render_template('frio.html', ingredients = ingredients)
 
+	# turns inputIngredientList into comma-separated String
+	result = ""
+	for word in inputIngredientList:
+		result += word + ","
+
+	# calls Test2 functions to create recipe dictionary json file
+	recipeDict = get_valid_recipes(result)
+
+	# list for just recipe titles
+	recipenames = []
+
+	# adds to list of possible recipes
+	for recipe in recipeDict:
+		recipenames.append(recipe['title'])
+
+	# connects to frio html file from our database and lists
+	return render_template('frio.html', ingredients = ingredients, recipenames = recipenames)
+
+# when hitting submit on adding ingredients
 @app.route('/add', methods = ['POST'])
 def add():
+	# adds to ingredient database
 	ingredient = Ingredients(text = request.form['ingredient'])
 	db.session.add(ingredient)
 	db.session.commit()
+
+	# loads all ingredient objects into ings 
 	ings = Ingredients.query.all()
+
+	# for each ingredient, aggregate array of letters into one word and append to indgredient list
 	for ing in ings:
 		word = "".join(ing.text)
 		if word not in inputIngredientList:
 			inputIngredientList.append(word)
-
-	#print_names(ingr_to_recipes(inputIngredientList))
-	print(inputIngredientList)
+	# don't switch pages after submission
 	return redirect(url_for('frio'))
 
+# way to remove ingredients from database with buttons
 @app.route('/gone/<id>')
 def gone(id):
+	# find the first ingredient that is tagged by ingredient ID
 	ingredient = Ingredients.query.filter_by(id = int(id)).first()
+	# deletes ingredient from database
 	db.session.delete(ingredient)
 	db.session.commit()
+	# makes a word out of word to be deleted
 	word = "".join(ingredient.text)
+	#removes word from list to use with API
 	inputIngredientList.remove(word)
+	#sanity check 0:
 	print(inputIngredientList)
 	return redirect(url_for('frio'))
 
